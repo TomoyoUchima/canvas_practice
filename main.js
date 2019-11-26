a9c274945434ba601b2d2b5fe0840d6c5848da0a
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-11-26 14:52:59 +0900

    複数の図形を描画

--- main.js
+++ main.js
@@ -8,16 +8,18 @@
     }
      const ctx = canvas.getContext('2d');  //絵筆のようなもの(色設定や、描画)
 
-    //  ctx.fillRect(x, y, width, height);
     ctx.fillStyle = 'pink';  //塗りつぶしの色
-     ctx.fillRect(50, 50, 50, 50);
-     ctx.strokeStyle = '#f00';  //枠線の色
-     ctx.lineWidth = 8; //線の太さ
-    //  ctx.lineJoin = 'round';
-     ctx.lineJoin = 'bevel';
-     ctx.strokeRect(50, 50, 50, 50);
-  }
+    ctx.strokeStyle = '#f00';  //枠線の色
 
+     ctx.fillRect(50, 50, 50, 50);
+     ctx.strokeRect(50, 50, 50, 50);
+
+    ctx.fillStyle = 'skyblue';
+    ctx.strokeStyle = '#00b'
+
+     ctx.fillRect(70, 70, 50, 50);
+     ctx.strokeRect(70, 70, 50, 50);
+  }
 
   draw();  
 }
