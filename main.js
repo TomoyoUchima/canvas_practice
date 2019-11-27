3f88e9467cbc256080ab318437f5d4f7d3f64162
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-11-27 15:03:43 +0900

    影の設定

--- main.js
+++ main.js
@@ -6,19 +6,13 @@
     if (typeof canvas.getContext === 'undefined') {
       return;
     }
-     const ctx = canvas.getContext('2d');  //絵筆のようなもの(色設定や、描画)
+     const ctx = canvas.getContext('2d');  
 
-     //ctx.createLinearGradient(x0, y0, x1(終点), y1(終点));
-     const g = ctx.createLinearGradient(0, 0, canvas.width,0);
-
-     g.addColorStop(0, '#f00'); //始点(0)に赤色を配置
-     g.addColorStop(0.3, '#0f0');
-     g.addColorStop(1, '#00f'); //終点(1)に青色を配置
-
-     ctx.fillStyle = g;
-
-    //  ctx.fillRect(0, 0, 600, 240);
-     ctx.fillRect(0, 0, canvas.width, canvas.height);
+     ctx.shadowOffsetX = 4;
+     ctx.shadowOffsetY = 4;
+     ctx.shadowBlur = 4;
+     ctx.shadowColor = 'rgba(0, 0, 0, 0.3)';
+     ctx.fillRect(50, 50, 50, 50);
 
   }
 
