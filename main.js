4c445e75f4b6ddf9bc315da869ca6b0950b2710c
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-11-28 11:49:57 +0900

    楕円の描画

--- main.js
+++ main.js
@@ -8,22 +8,13 @@
     }
      const ctx = canvas.getContext('2d');  
 
-    ctx.beginPath();
-    // tx.arc(x, y, r, start, end);
-    // x,y → 円の中心の位置、  r → 半径　　360° = 2π
-    // 0°が右端、時計回りに角度が増えていく
-    // ctx.arc(100, 100, 50, 0, 2 * Math.PI);　//円
-    // ctx.arc(100, 100, 50, 0, 300 / 360 * 2 * Math.PI); //300°の場
-    // ctx.arc(100, 100, 50, 0, 300 / 180 * Math.PI); //300°の場合(約分)
-    ctx.moveTo(100, 100);
-    ctx.arc(100, 100, 50, 0, 300 / 180 * Math.PI, true); //扇型
-    
-
-    //最後の引数に true を与えると反時計周りにしてくれる。
-    // ctx.stroke(); 
-    ctx.fill(); //塗りつぶしの場合
-
-    
+     // ctx.ellipse(x, r, rx, ry, rotation, start, end);
+    //  x、y → 中心のxy座標、rx、ry → x、y各半径、 残りはそれぞれの角度
+    //  beginPathは省略可能
+    //  ctx.ellipse(100, 100, 50, 30, 0, 0, 2 * Math.PI);
+    ctx.rect(50, 50, 50, 50);
+    // ctx.fillRect(50, 50, 50, 50); ↑と同じ
+     ctx.stroke();
 
   }
 
