ec918bad26b4b9a9d646bbfe8de1bd554874eeca
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-11-28 10:28:55 +0900

    線のスタイルの設定

--- main.js
+++ main.js
@@ -9,13 +9,24 @@
      const ctx = canvas.getContext('2d');  
 
      ctx.beginPath();
-     ctx.moveTo(50, 50);  //開始位置
-     ctx.lineTo(100, 50); //線を引く命令(真横に線を弾きたいのでx方向だけ大きく
-     ctx.lineTo(100, 100); //線を引く命令
-     ctx.closePath(); //線を閉じる
-     ctx.fill(); //塗りつぶし
-    //  ctx.stroke();  //描画
+     ctx.moveTo(100, 50);
+     ctx.lineTo(200, 50);
+     ctx.setLineDash([5, 10]); //線を引く長さと空白を交互に指定
+     ctx.stroke();
 
+     ctx.beginPath();
+     ctx.moveTo(100, 100);
+     ctx.lineTo(200, 100);
+     ctx.setLineDash([]);  //上の点線が引き継がれるため、実線にするには空の配列を指定
+     ctx.stroke();
+
+
+     ctx.beginPath();
+     ctx.moveTo(100, 150);
+     ctx.lineTo(200, 150);
+     ctx.lineWidth = 16;
+     ctx.lineCap = 'round';
+     ctx.stroke();
   }
 
   draw();  
