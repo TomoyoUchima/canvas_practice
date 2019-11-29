cbecac8c0c49c34920d979c71890f74be7ea2638
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-11-29 15:16:47 +0900

    アニメーション

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 'use strict';
 
 {
+let t = 0;
+
   function draw() {
     const canvas = document.querySelector('canvas');
     if (typeof canvas.getContext === 'undefined') {
       return;
     }
      const ctx = canvas.getContext('2d');
-     
-     ctx.fillStyle = 'pink';
-     ctx.fillRect(0, 0, 200, 200);
+
+//clearRectは、四角で領域をクリアすることができるので Canvas領域いっぱいを draw()関数を実行する度にクリアしてあげる。
+     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
 
      ctx.beginPath();
      ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI); //2π = 360°
@@ -17,38 +20,17 @@
      ctx.fill();
 
      ctx.beginPath();
-     ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI); 
-     ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI); 
+    //  ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI); 
+    //  ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI); 
+    //三角関数を使えば -1 から 1 で変化する値を取得できるので、目の x 座標に足してあげる。(アニメーションをつける)
+     ctx.ellipse(80 + Math.sin(t /30), 100, 8, 8, 0, 0, 2 * Math.PI); 
+     ctx.ellipse(120 + Math.sin(t /30), 100, 8, 8, 0, 0, 2 * Math.PI); 
      ctx.fillStyle = 'skyblue';
      ctx.fill();
 
-     ctx.save();
+      t++;
+      setTimeout(draw, 10); //draw関数を10mm秒後に呼び出してあげる
 
-//同じキャラクターを描きたい場合、また座標を計算するのがめんどくさい場合
-     ctx.scale(0.5, 0.5);  //xy方向の倍率を指定できる
-     ctx.translate(400, 0);  //座標空間を移動させる
-     ctx.rotate(45 /360 * Math.PI) //ラジアン
-     console.log(45 /360 * Math.PI)
-
-     ctx.fillStyle = 'olive';
-     ctx.fillRect(0, 0, 200, 200);
-
-
-     ctx.beginPath();
-     ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
-     ctx.fillStyle = 'black';
-     ctx.fill();
-
-     ctx.beginPath();
-     ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI); 
-     ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI); 
-     ctx.fillStyle = 'skyblue';
-     ctx.fill();
-
-     ctx.restore();
-
-     ctx.fillStyle = 'black';
-     ctx.fillRect(80, 120, 40, 40);
   }
 
   draw();  
