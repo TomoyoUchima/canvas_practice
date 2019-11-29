eb15f999dcce943665b83d24f784da0966d15c59
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-11-29 14:10:32 +0900

    座標空間を変形させる

--- main.js
+++ main.js
@@ -6,7 +6,10 @@
     if (typeof canvas.getContext === 'undefined') {
       return;
     }
-     const ctx = canvas.getContext('2d');  
+     const ctx = canvas.getContext('2d');
+     
+     ctx.fillStyle = 'pink';
+     ctx.fillRect(0, 0, 200, 200);
 
      ctx.beginPath();
      ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI); //2π = 360°
@@ -14,23 +17,29 @@
      ctx.fill();
 
      ctx.beginPath();
-     ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI); //2π = 360°
-     ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI); //2π = 360°
+     ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI); 
+     ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI); 
      ctx.fillStyle = 'skyblue';
      ctx.fill();
 
 //同じキャラクターを描きたい場合、また座標を計算するのがめんどくさい場合
      ctx.scale(0.5, 0.5);  //xy方向の倍率を指定できる
+     ctx.translate(400, 0);  //座標空間を移動させる
+     ctx.rotate(45 /360 * Math.PI) //ラジアン
+     console.log(45 /360 * Math.PI)
+
+     ctx.fillStyle = 'olive';
+     ctx.fillRect(0, 0, 200, 200);
 
 
      ctx.beginPath();
-     ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI); //2π = 360°
+     ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI);
      ctx.fillStyle = 'black';
      ctx.fill();
 
      ctx.beginPath();
-     ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI); //2π = 360°
-     ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI); //2π = 360°
+     ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI); 
+     ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI); 
      ctx.fillStyle = 'skyblue';
      ctx.fill();
 
