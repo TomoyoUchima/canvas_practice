d9247afb106ed9d64d061b5992616dacdcff8a37
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-11-29 12:22:15 +0900

    画像の一部を切り出す

--- main.js
+++ main.js
@@ -9,20 +9,19 @@
      const ctx = canvas.getContext('2d');  
 
      const img = document.createElement('img');
-     img.src = 'img/logo.png';　//ソース指定
+     img.src = 'img/sprite.png'
 
-     //画像の場合、読み込みが終わった場合に描画させたい
      img.addEventListener('load', () => {
-      //  ctx.drawImage(img, 0, 0); //左上に表示
-      //  ctx.drawImage(img, 0, 0, 40, 40); //幅指定可能
-      //  const pattern = ctx.createPattern(img, 'repeat'); //cteatePattern → 画像を塗りに設定、　repeat → 縦横方向に画像を繰り返す形で敷き詰められる
-      // 一部の方向だけ繰り返したい場合 → repeat-x, repeat-y, no-repeat  ↓例
-      const pattern = ctx.createPattern(img, 'repeat-y');
-       ctx.fillStyle = pattern;
-       ctx.fillRect(0, 0, canvas.width, canvas.height);
-     });
+       ctx.drawImage(img, 0, 0);
+       ctx.drawImage(
+        img, 
+        //sx, sy, sw, wh → 描画元(source)
+        //dx, dy, dw, dh → 表示先(destination)
+        70 * 2, 70, 70, 70,
+        0, 0, 35, 35
+       )
 
-    //  fillRect
+     });
   }
 
   draw();  
