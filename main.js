edace4b29c30c7984227979826955b55119bdd02
TomoyoUchima <tomoyo.uchima@cydas.com>
2019-12-02 10:58:01 +0900

    高解像度ディスプレイ対応させる

--- main.js
+++ main.js
@@ -10,26 +10,24 @@ let t = 0;
     }
      const ctx = canvas.getContext('2d');
 
-//clearRectは、四角で領域をクリアすることができるので Canvas領域いっぱいを draw()関数を実行する度にクリアしてあげる。
-     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //  解像度の密度に応じて、いったん大きな領域に描画したあとに、それをぎゅっと縮小してきれいな表示にさせる。
 
+    const CANVAS_WIDTH = 600;
+    const CANVAS_HEIGHT = 240;
+    //devicePixelRatio → ディスプレイの密度を取得。描画するときの 1px が物理ピクセルいくつ分かを取得できる(現在のディスプレイにおけるCSS解像度と物理解像度の比を返す)。
+    //※ ||1 → devicePixelRatioが取得できなかった場合は等倍である 1 を返しなさい、という命令。
+    const dpr = window.devicePixelRatio || 1; 
+    //Canvas の領域をいったん拡大。(倍の密度だったらCanvasが2倍になるという意味)
+    canvas.width = CANVAS_WIDTH * dpr; 
+    canvas.height = CANVAS_HEIGHT * dpr;
+     // Canvas を大きくしただけだと図形は大きくならないので、ここで変形のための scale() を使う。
+    ctx.scale(dpr, dpr);
+    //表示上の大きさを変えるには canvas の style プロパティの width などを変えてあげる。そうすると密度はそのままに半分の大きさにしてくれる。
+    canvas.style.width = CANVAS_WIDTH + 'px'; 
+    canvas.style.height = CANVAS_HEIGHT + 'px';
 
-     ctx.beginPath();
-     ctx.ellipse(100, 100, 40, 30, 0, 0, 2 * Math.PI); //2π = 360°
-     ctx.fillStyle = 'black';
-     ctx.fill();
-
-     ctx.beginPath();
-    //  ctx.ellipse(80, 100, 8, 8, 0, 0, 2 * Math.PI); 
-    //  ctx.ellipse(120, 100, 8, 8, 0, 0, 2 * Math.PI); 
-    //三角関数を使えば -1 から 1 で変化する値を取得できるので、目の x 座標に足してあげる。(アニメーションをつける)
-     ctx.ellipse(80 + Math.sin(t /30), 100, 8, 8, 0, 0, 2 * Math.PI); 
-     ctx.ellipse(120 + Math.sin(t /30), 100, 8, 8, 0, 0, 2 * Math.PI); 
-     ctx.fillStyle = 'skyblue';
-     ctx.fill();
-
-      t++;
-      setTimeout(draw, 10); //draw関数を10mm秒後に呼び出してあげる
+    ctx.font = 'bold 48px Verdana';
+    ctx.strokeText('Tokyo', 100, 100); 
 
   }
 
